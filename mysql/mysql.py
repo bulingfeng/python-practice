949ea7cd99281c79a60161bec386371f5c2e91f6
bulingfeng <413957294@qq.com>
2022-08-15 17:44:50 +0800

    基本查询

---
+++ mysql/mysql.py
@@ -0,0 +1,20 @@
+import pymysql as mysql
+
+db = mysql.connect(host='127.0.0.1',
+                   user='root',
+                   password='1234567',
+                   database='e_commerce')
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# 使用 execute()  方法执行 SQL 查询
+cursor.execute("select * from t_ecommerce_user ")
+
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchall()
+# 打印
+print(data)
+
+# 关闭数据库连接
+db.close()
